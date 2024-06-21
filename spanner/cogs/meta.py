04898a5925c56825a9312e9accbd102deb72aa4d
nexy7574 <git@nexy7574.co.uk>
2024-06-21 01:03:54 +0100

    Add update check

--- spanner/cogs/meta.py
+++ spanner/cogs/meta.py
@@ -1,5 +1,7 @@
 import discord
+import httpx
 from discord.ext import bridge, commands
+from httpx import AsyncClient
 
 from spanner.share.config import load_config
 from spanner.share.database import GuildLogFeatures
@@ -57,11 +59,25 @@ class MetaCog(commands.Cog):
     async def version(self, ctx: commands.Context):
         """Gets the spanner version."""
         from spanner.share.version import __sha__, __sha_short__, __build_time__
-        url = "https://github.com/nexy7574/spanner-v3/tree/{}".format(__sha__)
+        base_url = "https://github.com/nexy7574/spanner-v3/tree/{}"
+        url = base_url.format(__sha__)
         ts = discord.utils.format_dt(__build_time__, 'R')
-        return await ctx.reply(
+        msg = await ctx.reply(
             f"Running [Spanner v3, commit `{__sha_short__}`](<{url}>) built {ts}.",
         )
+        async with httpx.AsyncClient() as client:
+            response = await client.get("https://git.i-am.nexus/api/v1/repos/nex/spanner-v3/commits?sha=dev")
+            response.raise_for_status()
+            commits = response.json()
+            latest = commits.pop(0)
+            if latest["sha"] != __sha__:
+                await msg.edit(
+                    content="{0}\n\nA newer version is available: [commit `{1}`]({2})".format(
+                        msg.content,
+                        latest["sha"][:7],
+                        base_url.format(latest["sha"])
+                    )
+                )
 
 
 def setup(bot: commands.Bot):
