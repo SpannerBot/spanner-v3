231030f63d5e3699282d1556400a4e68351a4557
nexy7574 <git@nexy7574.co.uk>
2024-07-16 15:47:18 +0100

    Fix skus create

--- spanner/cogs/dev_entitlements.py
+++ spanner/cogs/dev_entitlements.py
@@ -9,6 +9,7 @@ class DevEntitlementCog(commands.Cog):
     @commands.group(name="entitlements", aliases=['ent'], invoke_without_command=True)
     @commands.is_owner()
     async def entitlements(self, ctx):
+        """entitlements management"""
         return
 
     @entitlements.command(name="list")
@@ -36,15 +37,16 @@ class DevEntitlementCog(commands.Cog):
 
         async for entitlement in entitlements:
             paginator.add_line(
-                "{0.id} - sku_id={0.sku_id!r} user_id={0.user_id!r} type={0.type.name!r} deleted={0.deleted!r} "
+                "{0.id} - sku_id={0.sku_id!r} user_id={0.user_id!r} type={0.type!r} deleted={0.deleted!r} "
                 "starts_at={0.starts_at!r} ends_at={0.ends_at!r} guild_id={0.guild_id!r} "
-                "consumed={0.consumed!r}".format(entitlement)
+                "consumed={0.consumed!r}".format(entitlement),
+                empty=True
             )
 
         if not paginator.pages:
-            return await ctx.send("No entitlements found.")
+            return await ctx.reply("No entitlements found.")
         for page in paginator.pages:
-            await ctx.send(page)
+            await ctx.reply(page)
 
     @entitlements.command(name="create")
     @commands.is_owner()
@@ -68,14 +70,17 @@ class DevEntitlementCog(commands.Cog):
             return await ctx.reply("SKU not found.")
         sku_id = sku.id
 
-        entitlement = await self.bot.http.create_test_entitlement(
-            self.bot.user.id,
-            discord.monetization.CreateTestEntitlementPayload(
-                sku_id=sku_id,
-                owner_id=owner.id,
-                owner_type=owner_type
+        try:
+            entitlement = await self.bot.http.create_test_entitlement(
+                self.bot.user.id,
+                dict(
+                    sku_id=sku_id,
+                    owner_id=owner.id,
+                    owner_type=owner_type
+                )
             )
-        )
+        except discord.HTTPException as e:
+            return await ctx.reply(f"Failed to create entitlement: {e!r}")
         return await ctx.reply(f"Entitlement created: {entitlement!r}")
 
     @entitlements.command(name="delete")
@@ -86,33 +91,41 @@ class DevEntitlementCog(commands.Cog):
             entitlement_id: int
     ):
         """Deletes a test entitlement"""
-        await self.bot.http.delete_test_entitlement(self.bot.user.id, entitlement_id)
+        try:
+            await self.bot.http.delete_test_entitlement(self.bot.user.id, entitlement_id)
+        except discord.HTTPException as e:
+            return await ctx.reply(f"Failed to delete entitlement: {e!r}")
         return await ctx.reply("Entitlement deleted.")
 
     @commands.group(name="skus", aliases=['sku'], invoke_without_command=True)
     @commands.is_owner()
     async def skus(self, ctx):
+        """SKU management"""
         return
 
     @skus.command(name="list")
     @commands.is_owner()
     async def list_skus(self, ctx: commands.Context):
         """Lists all SKUs available."""
-        skus = await self.bot.fetch_skus()
+        try:
+            skus = await self.bot.fetch_skus()
+        except discord.HTTPException as e:
+            return await ctx.reply(f"Failed to fetch SKUs: {e!r}")
         paginator = commands.Paginator()
 
         for sku in skus:
             paginator.add_line(
-                "{0.id} - name={0.name!r} type={0.type.name!r} slug={0.slug!r} flags={1}".format(
+                "{0.id} - name={0.name!r} type={0.type!r} slug={0.slug!r} flags={1}".format(
                     sku,
                     dict(sku.flags)
-                )
+                ),
+                empty=True
             )
 
         if not paginator.pages:
-            return await ctx.send("No SKUs found.")
+            return await ctx.reply("No SKUs found.")
         for page in paginator.pages:
-            await ctx.send(page)
+            await ctx.reply(page)
 
 
 def setup(bot):
